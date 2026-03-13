d54b96ddaf5ad8dffa3601a16d78c4a729f30874
buttonrig <252117212+buttonrig@users.noreply.github.com>
2026-03-13 23:19:43 +0530

    more sdk functions

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,146 @@ import {
   newMessage,
   type RxPayload,
   type TxPayload,
-  ErrorPayload,
-  newSaveHandlerArgs,
-  RxLoadHandlerArgsPayload,
-  RxFilePickPayload,
-  RxFolderPickPayload,
-  RxFilesPickPayload,
+  type RxLoadHandlerArgs,
+  type RxFilePick,
+  type RxFolderPick,
+  type RxFilesPick,
+  type RxCallHandler,
+  type CallHandlerResponse,
+  type RxGetPluginKeyValue,
+  type RxGetInstanceKeyValue,
+  DistributionTarget,
+  type RxGetCurrentTarget,
 } from "./types.js";
 
-export function saveHandlerArgs(args: string[] | ErrorPayload) {
-  let txPayload: TxPayload;
-  if (args instanceof ErrorPayload) {
-    txPayload = args;
-  } else {
-    txPayload = newSaveHandlerArgs(args);
-  }
-  postMessage(txPayload);
+export function getPluginKeyValue<T>(key: string): Promise<T | null> {
+  return new Promise((resolve) => {
+    postMessage({
+      getPluginKeyValue: {
+        key,
+      },
+    });
+    addEventListener("getPluginKeyValue", (rxPayload) => {
+      let payload = rxPayload as RxGetPluginKeyValue;
+      if (
+        payload.getPluginKeyValue.value !== null &&
+        payload.getPluginKeyValue.value !== undefined
+      ) {
+        resolve(JSON.parse(payload.getPluginKeyValue.value) as T);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+}
+
+export function savePluginKeyValue<T>(key: string, value: T) {
+  postMessage({
+    savePluginKeyValue: {
+      key,
+      value: JSON.stringify(value),
+    },
+  });
+}
+
+export function removePluginKeyValue(key: string) {
+  postMessage({
+    removePluginKeyValue: {
+      key,
+    },
+  });
+}
+
+export function getInstanceKeyValue<T>(key: string): Promise<T | null> {
+  return new Promise((resolve) => {
+    postMessage({
+      getInstanceKeyValue: {
+        key,
+      },
+    });
+
+    addEventListener("getInstanceKeyValue", (rxPayload) => {
+      const payload = rxPayload as RxGetInstanceKeyValue;
+      if (
+        payload.getInstanceKeyValue.value !== null &&
+        payload.getInstanceKeyValue.value !== undefined
+      ) {
+        resolve(JSON.parse(payload.getInstanceKeyValue.value) as T);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+}
+
+export function saveInstanceKeyValue<T>(key: string, value: T) {
+  postMessage({
+    saveInstanceKeyValue: {
+      key,
+      value: JSON.stringify(value),
+    },
+  });
+}
+
+export function removeInstanceKeyValue(key: string) {
+  postMessage({
+    removeInstanceKeyValue: {
+      key,
+    },
+  });
+}
+
+export function callHandler(args: string[]): Promise<CallHandlerResponse> {
+  return new Promise((resolve) => {
+    postMessage({
+      callHandler: {
+        handlerArgs: args,
+      },
+    });
+    addEventListener("callHandler", (rxPayload) => {
+      let payload = rxPayload as RxCallHandler;
+      resolve(payload.callHandler);
+    });
+  });
+}
+
+export function getCurrentTarget(): Promise<DistributionTarget | null> {
+  return new Promise((resolve) => {
+    postMessage("getCurrentTarget");
+    addEventListener("getCurrentTarget", (rxPayload) => {
+      let payload = rxPayload as RxGetCurrentTarget;
+      resolve(payload.getCurrentTarget.target);
+    });
+  });
+}
+
+export function setConfiguratorHeight(height_in_px: number) {
+  postMessage({
+    setConfiguratorHeight: {
+      height: height_in_px,
+    },
+  });
+}
+
+export function setError(error: string) {
+  postMessage({
+    error: {
+      message: error,
+    },
+  });
+}
+
+export function saveHandlerArgs(args: string[]) {
+  postMessage({
+    saveHandlerArgs: {
+      handlerArgs: args,
+    },
+  });
 }
 
 export function loadHandlerArgs(fn: (handlerArgs: string[]) => void) {
   addEventListener("loadHandlerArgs", (payload) => {
-    let loadHandlerArgsPayload = payload as RxLoadHandlerArgsPayload;
+    let loadHandlerArgsPayload = payload as RxLoadHandlerArgs;
     fn(loadHandlerArgsPayload.loadHandlerArgs.handlerArgs);
   });
   postMessage("readyToReceive");
@@ -35,9 +154,9 @@ export function pickFile(extensions: string[]): Promise<string | null> {
         extensions,
       },
     });
-    addEventListener("filePick", (payload) => {
-      let filePickPayload = payload as RxFilePickPayload;
-      resolve(filePickPayload.filePick.file);
+    addEventListener("filePick", (rxPayload) => {
+      let payload = rxPayload as RxFilePick;
+      resolve(payload.filePick.file);
     });
   });
 }
@@ -49,9 +168,9 @@ export function pickFiles(extensions: string[]): Promise<string[]> {
         extensions,
       },
     });
-    addEventListener("filesPick", (payload) => {
-      let filesPickPayload = payload as RxFilesPickPayload;
-      resolve(filesPickPayload.filesPick.files);
+    addEventListener("filesPick", (rxPayload) => {
+      let payload = rxPayload as RxFilesPick;
+      resolve(payload.filesPick.files);
     });
   });
 }
@@ -60,13 +179,16 @@ export function pickFolder(): Promise<string | null> {
   return new Promise((resolve) => {
     postMessage("pickFolder");
     addEventListener("folderPick", (payload) => {
-      let folderPickPayload = payload as RxFolderPickPayload;
+      let folderPickPayload = payload as RxFolderPick;
       resolve(folderPickPayload.folderPick.folder);
     });
   });
 }
 
-function addEventListener(eventType: string, fn: (payload: RxPayload) => void) {
+function addEventListener(
+  eventType: string,
+  fn: (rxPayload: RxPayload) => void,
+) {
   window.addEventListener("message", (event) => {
     if (
       !(
@@ -80,7 +202,7 @@ function addEventListener(eventType: string, fn: (payload: RxPayload) => void) {
   });
 }
 
-function postMessage(payload: TxPayload) {
-  let message = newMessage(payload);
+function postMessage(txPayload: TxPayload) {
+  let message = newMessage(txPayload);
   window.parent.postMessage(message, "*");
 }
--- src/types.ts
+++ src/types.ts
@@ -22,13 +22,163 @@ export function newMessage(payload: TxPayload): Message {
   };
 }
 
+export interface CallHandlerResponse {
+  response: string;
+  exitCode: number | null;
+}
+
+export enum DistributionTarget {
+  Windows_X86_64 = "Windows_X86_64",
+  Linux_X86_64 = "Linux_X86_64",
+}
+
+export type RxPayload =
+  | RxFilePick
+  | RxFilesPick
+  | RxFolderPick
+  | RxLoadHandlerArgs
+  | RxCallHandler
+  | RxGetPluginKeyValue
+  | RxGetInstanceKeyValue
+  | RxGetCurrentTarget
+  | null;
+
+export interface RxGetCurrentTarget {
+  getCurrentTarget: {
+    target: DistributionTarget | null;
+  };
+}
+
+export interface RxGetInstanceKeyValue {
+  getInstanceKeyValue: {
+    value: string | null | undefined;
+  };
+}
+
+export interface RxGetPluginKeyValue {
+  getPluginKeyValue: {
+    value: string | null | undefined;
+  };
+}
+
+export interface RxCallHandler {
+  callHandler: CallHandlerResponse;
+}
+
+export interface RxLoadHandlerArgs {
+  loadHandlerArgs: {
+    handlerArgs: string[];
+  };
+
+  // constructor(handlerArgs: string[]) {
+  //   this.loadHandlerArgs = {
+  //     handlerArgs,
+  //   };
+  // }
+}
+
+export interface RxFilePick {
+  filePick: {
+    file: string | null;
+  };
+
+  // constructor() {
+  //   this.filePick = {
+  //     file: null,
+  //   };
+  // }
+}
+
+export interface RxFilesPick {
+  filesPick: {
+    files: string[];
+  };
+
+  // constructor() {
+  //   this.filesPick = {
+  //     files: [],
+  //   };
+  // }
+}
+
+export interface RxFolderPick {
+  folderPick: {
+    folder: string | null;
+  };
+
+  // constructor() {
+  //   this.folderPick = {
+  //     folder: null,
+  //   };
+  // }
+}
+
 export type TxPayload =
+  | TxCallHandler
+  | TxSavePluginKeyValue
+  | TxRemovePluginKeyValue
+  | TxGetPluginKeyValue
+  | TxSaveInstanceKeyValue
+  | TxRemoveInstanceKeyValue
+  | TxGetInstanceKeyValue
+  | TxGetCurrentTarget
+  | TxSetConfiguratorHeight
   | TxPickFilePayload
   | TxPickFilesPayload
   | TxPickFolder
   | TxReadyToReceive
   | TxSaveHandlerArgsPayload
   | ErrorPayload;
+
+export interface TxSetConfiguratorHeight {
+  setConfiguratorHeight: {
+    height: number;
+  };
+}
+
+export interface TxGetInstanceKeyValue {
+  getInstanceKeyValue: {
+    key: string;
+  };
+}
+
+export interface TxRemoveInstanceKeyValue {
+  removeInstanceKeyValue: {
+    key: string;
+  };
+}
+
+export interface TxSaveInstanceKeyValue {
+  saveInstanceKeyValue: {
+    key: string;
+    value: string;
+  };
+}
+
+export interface TxGetPluginKeyValue {
+  getPluginKeyValue: {
+    key: string;
+  };
+}
+
+export interface TxRemovePluginKeyValue {
+  removePluginKeyValue: {
+    key: string;
+  };
+}
+
+export interface TxSavePluginKeyValue {
+  savePluginKeyValue: {
+    key: string;
+    value: string;
+  };
+}
+
+export interface TxCallHandler {
+  callHandler: {
+    handlerArgs: string[];
+  };
+}
 
 export interface TxPickFilePayload {
   pickFile: {
@@ -39,72 +189,7 @@ export interface TxPickFilePayload {
 export interface TxPickFilesPayload {
   pickFiles: {
     extensions: string[];
-  }
-}
-
-export function newSaveHandlerArgs(
-  handlerArgs: string[]
-): TxSaveHandlerArgsPayload {
-  return {
-    saveHandlerArgs: {
-      handlerArgs,
-    },
   };
-}
-
-export type RxPayload =
-  | RxFilePickPayload
-  | RxFilesPickPayload
-  | RxFolderPickPayload
-  | RxLoadHandlerArgsPayload
-  | null;
-
-export class RxLoadHandlerArgsPayload {
-  loadHandlerArgs: {
-    handlerArgs: string[];
-  };
-
-  constructor(handlerArgs: string[]) {
-    this.loadHandlerArgs = {
-      handlerArgs,
-    };
-  }
-}
-
-export class RxFilePickPayload {
-  filePick: {
-    file: string | null;
-  };
-
-  constructor() {
-    this.filePick = {
-      file: null,
-    };
-  }
-}
-
-export class RxFilesPickPayload {
-  filesPick: {
-    files: string[];
-  };
-
-  constructor() {
-    this.filesPick = {
-      files: [],
-    };
-  }
-}
-
-export class RxFolderPickPayload {
-  folderPick: {
-    folder: string | null;
-  };
-
-  constructor() {
-    this.folderPick = {
-      folder: null,
-    };
-  }
 }
 
 export interface TxSaveHandlerArgsPayload {
@@ -115,3 +200,4 @@ export interface TxSaveHandlerArgsPayload {
 
 export type TxReadyToReceive = "readyToReceive";
 export type TxPickFolder = "pickFolder";
+export type TxGetCurrentTarget = "getCurrentTarget";
